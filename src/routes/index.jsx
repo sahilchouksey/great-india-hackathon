20c6282d4e169de187f3de29281e77daeaa6c1bc
pradeeppatel123 <pp0608105@gmail.com>
2023-04-22 15:13:23 +0530

    pradeep

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -2,12 +2,12 @@ import { Routes, Route } from "react-router-dom";
 import Home from "../containers/Home";
 
 import Layout from "../components/layout";
-import { ROUTES } from "./data";
+import { ROUTES as ROUTES_ } from "./data";
 export default function Routing() {
   return (
     <Layout>
       <Routes>
-        <Route exact path={ROUTES.home} element={<Home />} />
+        <Route exact path={ROUTES_.home} element={<Home />} />
       </Routes>
     </Layout>
   );
